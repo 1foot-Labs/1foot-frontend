193c3d788d617e9dd3a400c6d3eed94f7ebfac43
raehat <raehatsinghnanda@gmail.com>
2025-08-01 09:47:53 +0530

    add has160 and sha3 to secret generation

--- app/utils/secrets.ts
+++ app/utils/secrets.ts
@@ -2,14 +2,14 @@ import crypto from 'crypto';
 import { keccak256 } from 'js-sha3';
 
 export function generateSecret(): {
-  secret: number;
-  sha256: string;
+  secret: string;
   hash160: string;
   sha3: string;
 } {
-  const secret = Math.floor(Math.random() * 256) + 1;
+  const secretInt = Math.floor(Math.random() * 256) + 1;
+  const secret = secretInt.toString()
 
-  const secretBuffer = Buffer.from([secret]);
+  const secretBuffer = Buffer.from([secretInt]);
 
   const sha256 = crypto.createHash('sha256').update(secretBuffer).digest();
   const hash160 = crypto.createHash('ripemd160').update(sha256).digest('hex');
@@ -17,7 +17,6 @@ export function generateSecret(): {
 
   return {
     secret,
-    sha256: sha256.toString('hex'),
     hash160,
     sha3
   };
